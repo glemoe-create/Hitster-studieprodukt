457bd0c0cd18d95dda0f4934e7275250e1940302
kathaldbo <kathaldbo@gmail.com>
2026-04-13 10:15:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,261 @@
+const sange = [
+    {
+        Titel: "Wannabe",
+        Kunstner: "Spice Girls",
+        Aarstal: 1996,
+        Link: "https://open.spotify.com/embed/track/1Je1IMUlBXcx1Fz0WE7oPT"
+    },
+    {
+        Titel: "I Want It That Way",
+        Kunstner: "Backstreet Boys",
+        Aarstal: 1999,
+        Link: "https://open.spotify.com/embed/track/47BBI51FKFwOMlIiX6m8ya"
+    },
+    {
+        Titel: "Baby One More Time",
+        Kunstner: "Britney Spears",
+        Aarstal: 1998,
+        Link: "https://open.spotify.com/embed/track/3MjUtNVVq3C8Fn0MP3zhXa"
+    },
+    {
+        Titel: "Oops!... I Did It Again",
+        Kunstner: "Britney Spears",
+        Aarstal: 1999,
+        Link: "https://open.spotify.com/embed/track/6naxalmIoLFWR0siv8dnQQ"
+    },
+    {
+        Titel: "Bohemian Rhapsody",
+        Kunstner: "Queen",
+        Aarstal: 1975,
+        Link: "https://open.spotify.com/embed/track/4u7EnebtmKWzUH433cf5Qv"
+    },
+    {
+        Titel: "Billie Jean",
+        Kunstner: "Michael Jackson",
+        Aarstal: 1982,
+        Link: "https://open.spotify.com/embed/track/5ChkMS8OtdzJeqyybCc9R5"
+    },
+    {
+        Titel: "Take On Me",
+        Kunstner: "a-ha",
+        Aarstal: 1985,
+        Link: "https://open.spotify.com/embed/track/2WfaOiMkCvy7F5fcp2zZ8L"
+    },
+    {
+        Titel: "Never Gonna Give You Up",
+        Kunstner: "Rick Astley",
+        Aarstal: 1987,
+        Link: "https://open.spotify.com/embed/track/4cOdK2wGLETKBW3PvgPWqT"
+    },
+    {
+        Titel: "Smells Like Teen Spirit",
+        Kunstner: "Nirvana",
+        Aarstal: 1991,
+        Link: "https://open.spotify.com/embed/track/1f3yAtsJtY87CTmM8RLnxf"
+    },
+    {
+        Titel: "Mr. Brightside",
+        Kunstner: "The Killers",
+        Aarstal: 2003,
+        Link: "https://open.spotify.com/embed/track/003vvx7Niy0yvhvHt4a68B"
+    },
+    {
+        Titel: "Poker Face",
+        Kunstner: "Lady Gaga",
+        Aarstal: 2008,
+        Link: "https://open.spotify.com/embed/track/1QV6tiMFM6fSOKOGLMHYYg?si=da322d6130da46c6"
+    },
+    {
+        Titel: "Rolling in the Deep",
+        Kunstner: "Adele",
+        Aarstal: 2010,
+        Link: "https://open.spotify.com/embed/track/4OSBTYWVwsQhGLF9NHvIbR?si=9ad111ed3041493d"
+    },
+    {
+        Titel: "Uptown Funk",
+        Kunstner: "Mark Ronson ft. Bruno Mars",
+        Aarstal: 2014,
+        Link: "https://open.spotify.com/embed/track/32OlwWuMpZ6b0aN2RZOeMS"
+    },
+    {
+        Titel: "Blinding Lights",
+        Kunstner: "The Weeknd",
+        Aarstal: 2019,
+        Link: "https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b"
+    },
+    {
+        Titel: "Barbie Girl",
+        Kunstner: "Aqua",
+        Aarstal: 1997,
+        Link: "https://open.spotify.com/embed/track/5ZrDlcxIDZyjOzHdYW1ydr?si=a4ef47e948044d47"
+    },
+    {
+        Titel: "Sweet Dreams (Are Made of This)",
+        Kunstner: "Eurythmics",
+        Aarstal: 1983,
+        Link: "https://open.spotify.com/embed/track/1TfqLAPs4K3s2rJMoCokcS"
+    },
+    {
+        Titel: "No Scrubs",
+        Kunstner: "TLC",
+        Aarstal: 1999,
+        Link: "https://open.spotify.com/embed/track/1KGi9sZVMeszgZOWivFpxs?si=784c739338d14b0e"
+    },
+    {
+        Titel: "Dancing Queen",
+        Kunstner: "ABBA",
+        Aarstal: 1976,
+        Link: "https://open.spotify.com/embed/track/0GjEhVFGZW8afUYGChu3Rr"
+    },
+    {
+        Titel: "Kom Tilbage Nu",
+        Kunstner: "Danseorkestret",
+        Aarstal: 1985,
+        Link: "https://open.spotify.com/embed/track/598JrQyHZ5oviov9RFjsM8?si=c7cc06cba50240a1"
+    },
+    {
+        Titel: "Hey Ya!",
+        Kunstner: "Outkast",
+        Aarstal: 2003,
+        Link: "https://open.spotify.com/embed/track/2PpruBYCo4H7WOBJ7Q2EwM"
+    },
+    {
+        Titel: "Waterloo",
+        Kunstner: "ABBA",
+        Aarstal: 1974,
+        Link: "https://open.spotify.com/embed/track/3Dy4REq8O09IlgiwuHQ3sk?si=5405471e049e4217"
+    },
+    {
+        Titel: "I Will Survive",
+        Kunstner: "Gloria Gaynor",
+        Aarstal: 1978,
+        Link: "https://open.spotify.com/embed/track/7rIovIsXE6kMn629b7kDig?si=9c3caff260f8410e"
+    },
+    {
+        Titel: "Gimme! Gimme! Gimme! (A Man After Midnight)",
+        Kunstner: "ABBA",
+        Aarstal: 1979,
+        Link: "https://open.spotify.com/embed/track/3vkQ5DAB1qQMYO4Mr9zJN6?si=45877a27594b45b1"
+    },
+    {
+        Titel: "Danse i Måneskin",
+        Kunstner: "Trine Dyrholm",
+        Aarstal: 1987,
+        Link: "https://open.spotify.com/embed/track/47f5VH4gBRdqkMtFctiveF?si=91ab4af53b414f3c"
+    },
+    {
+        Titel: "Paint It, Black",
+        Kunstner: "The Rolling Stones",
+        Aarstal: 1966,
+        Link: "https://open.spotify.com/embed/track/63T7DJ1AFDD6Bn8VzG6JE8?si=a7305861155f48dd"
+    },
+    {
+        Titel: "Jolene",
+        Kunstner: "Dolly Parton",
+        Aarstal: 1973,
+        Link: "https://open.spotify.com/embed/track/5fdhThPDe6jQQDqCyWrdAn?si=2415377af34e4aae"
+    },
+    {
+        Titel: "Dreams",
+        Kunstner: "Fleetwood Mac",
+        Aarstal: 1977,
+        Link: "https://open.spotify.com/embed/track/0ofHAoxe9vBkTCp2UQIavz?si=d82d116fa9354592"
+    },
+    {
+        Titel: "September",
+        Kunstner: "Earth, Wind & Fire",
+        Aarstal: 1978,
+        Link: "https://open.spotify.com/embed/track/2grjqo0Frpf2okIBiifQKs?si=ca7c3df8f1944d4d"
+    },
+    {
+        Titel: "Girls Just Want to Have Fun",
+        Kunstner: "Cyndi Lauper",
+        Aarstal: 1983,
+        Link: "https://open.spotify.com/embed/track/4y1LsJpmMti1PfRQV9AWWe?si=45853169d8bf42c8"
+    },
+    {
+        Titel: "I Wanna Dance with Somebody (Who Loves Me)",
+        Kunstner: "Whitney Houston",
+        Aarstal: 1987,
+        Link: "https://open.spotify.com/embed/track/2tUBqZG2AbRi7Q0BIrVrEj?si=54b6a626d9d74fef"
+    },
+    {
+        Titel: "U Can't Touch This",
+        Kunstner: "MC Hammer",
+        Aarstal: 1990,
+        Link: "https://open.spotify.com/embed/track/1B75hgRqe7A4fwee3g3Wmu?si=6b80b5e466064584"
+    },
+    {
+        Titel: "Stupid Man",
+        Kunstner: "Thomas Helmig",
+        Aarstal: 1994,
+        Link: "https://open.spotify.com/embed/track/5HjIGzesTt8dwTgy8z7KRf?si=f41d2bbf9126485a"
+    },
+    {
+        Titel: "Gangsta's Paradise",
+        Kunstner: "Coolio",
+        Aarstal: 1995,
+        Link: "https://open.spotify.com/embed/track/1DIXPcTDzTj8ZMHt3PDt8p?si=e6a177cd92c24f78"
+    },
+    {
+        Titel: "Lose Yourself",
+        Kunstner: "Eminem",
+        Aarstal: 2002,
+        Link: "https://open.spotify.com/embed/track/5Z01UMMf7V1o0MzF86s6WJ?si=295b62d5929d4a53"
+    },
+    {
+        Titel: "Umbrella",
+        Kunstner: "Rihanna ft. JAY-Z",
+        Aarstal: 2007,
+        Link: "https://open.spotify.com/embed/track/49FYlytm3dAAraYgpoJZux?si=764aafe46e5b4ef6"
+    },
+    {
+        Titel: "Somebody That I Used To Know",
+        Kunstner: "Gotye ft. Kimbra",
+        Aarstal: 2011,
+        Link: "https://open.spotify.com/embed/track/13b3kZAjZGltp76ed4u1m3?si=05703680867e4802"
+    },
+    {
+        Titel: "Happy",
+        Kunstner: "Pharrell Williams",
+        Aarstal: 2013,
+        Link: "https://open.spotify.com/embed/track/60nZcImufyMA1MKQY3dcCH?si=e57cb113e7a34949"
+    },
+    {
+        Titel: "Shape of You",
+        Kunstner: "Ed Sheeran",
+        Aarstal: 2017,
+        Link: "https://open.spotify.com/embed/track/7qiZfU4dY1lWllzX7mPBI3?si=dde057766ce94094"
+    },
+    {
+        Titel: "As It Was",
+        Kunstner: "Harry Styles",
+        Aarstal: 2022,
+        Link: "https://open.spotify.com/embed/track/4Dvkj6JhhA12EX05fT7y2e?si=8135aa5841c44b90"
+    }
+];
+
+let afspillerElement = document.getElementById("musikAfspiller");
+let startKnap = document.getElementById("startKnap");
+
+function traekSang() {
+    let tilfaeldigtIndex = Math.floor(Math.random() * sange.length);
+    let valgtSang = sange[tilfaeldigtIndex];
+    afspillerElement.src = valgtSang.Link;
+}
+startKnap.addEventListener("click", traekSang);
+
+// Hent elementer
+const afslorKnap = document.getElementById("afsloerKnap");
+const aarstalsTekst = document.getElementById("aarstalTekst");
+const hemmeligBoks = document.getElementById("hemmeligBoks");
+
+// Eksempel-årstal (senere kan det komme fra en sang)
+const aarstallet = 1998;
+
+// Når man klikker "Afslør årstal"
+afslorKnap.addEventListener("click", () => {
+    aarstalsTekst.textContent = aarstallet;
+    hemmeligBoks.style.display = "none";
+});
+``
